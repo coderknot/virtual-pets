106dcb194c1cd43ff522cbe41e4cbd2d5c33f31b
chris carr and grace stuart <chris+grace@>
2017-04-03 14:46:05 -0700

    add abstract class

---
+++ src/main/java/FireMonster.java
@@ -0,0 +1,74 @@
+import java.util.Timer;
+import org.sql2o.*;
+import java.util.List;
+
+public class FireMonster extends Monster {
+
+  public FireMonster(String name, int personId) {
+    this.name = name;
+    this.personId = personId;
+    playLevel = MAX_PLAY_LEVEL / 2;
+    sleepLevel = MAX_SLEEP_LEVEL / 2;
+    foodLevel = MAX_FOOD_LEVEL / 2;
+    fireLevel = MAX_FIRE_LEVEL / 2;
+    timer = new Timer();
+  }
+
+  @Override
+  public void depleteLevels(){
+    if (isAlive()){
+      playLevel--;
+      foodLevel--;
+      sleepLevel--;
+      fireLevel--;
+    }
+  }
+
+  @Override
+  public void depleteLevels(){
+    if (isAlive()){
+      playLevel--;
+      foodLevel--;
+      sleepLevel--;
+      fireLevel--;
+    }
+  }
+
+  public int getFireLevel() {
+    return fireLevel;
+  }
+
+  public void kindling() {
+    fireLevel++;
+  }
+
+  public void kindling(){
+    if (fireLevel >= MAX_PLAY_LEVEL){
+     throw new UnsupportedOperationException("You cannot give any more kindling!");
+    }
+    fireLevel++;
+  }
+
+  public static List<FireMonster> all() {
+    String sql = "SELECT * FROM monsters;";
+    try(Connection con = DB.sql2o.open()) {
+      return con.createQuery(sql).executeAndFetch(FireMonster.class);
+    }
+  }
+
+  public static FireMonster find(int id) {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "SELECT * FROM monsters where id=:id";
+      FireMonster monster = con.createQuery(sql)
+        .addParameter("id", id)
+        .executeAndFetchFirst(FireMonster.class);
+      return monster;
+    }
+  }
+
+
+
+
+
+
+}
